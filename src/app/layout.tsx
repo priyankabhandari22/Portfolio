6769777113b83a04031ce98971b127adfe4febde
priyankabhandari22 <bhandaripriyanka028@gmail.com>
2026-06-22 12:13:06 +0530

    feat: add Lenis smooth scroll with GSAP integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Orbitron, Rajdhani, Share_Tech_Mono } from "next/font/google";
 import BootScreen from "@/components/BootScreen";
+import SmoothScroll from "@/components/SmoothScroll";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -60,7 +61,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <BootScreen />
-        {children}
+        <SmoothScroll>{children}</SmoothScroll>
       </body>
     </html>
   );
---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      touchMultiplier: 1.5,
+      wheelMultiplier: 1,
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.lagSmoothing(1);
+    };
+  }, []);
+
+  return <div ref={wrapperRef}>{children}</div>;
+}
