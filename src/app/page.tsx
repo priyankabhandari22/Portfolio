31333cbbfe709b60e7414954e5b995ba0d24ada1
priyankabhandari22 <bhandaripriyanka028@gmail.com>
2026-06-22 18:57:46 +0530

    feat: hex tunnel background replacing particle network

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Cursor from "@/components/Cursor";
 import GridBackground from "@/components/GridBackground";
-import ParticleNetwork from "@/components/ParticleNetwork";
+import HexBackground from "@/components/HexBackground";
 import Ripple from "@/components/Ripple";
 import SectionDivider from "@/components/SectionDivider";
 
@@ -18,7 +18,7 @@ export default function Home() {
       <Cursor />
       <Ripple />
       <GridBackground />
-      <ParticleNetwork />
+      <HexBackground />
       <Header />
       <main className="relative z-10">
         <Hero />
